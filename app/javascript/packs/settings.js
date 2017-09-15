c2e112b31f5c2156121480c3cedd8061ff5ba77a
Jimmy Castiglione <jimmyyoshi@gmail.com>
2017-09-14 21:15:36 -0400

    Adds buffer to make sure 59 gets shown
    Before, it was possible that the 24:59 would be skipped. This adds a
    100ms buffer to make sure that this number gets shown.

--- app/javascript/packs/settings.js
+++ app/javascript/packs/settings.js
@@ -1,2 +1,3 @@
-export const TIMER_LENGTH_MS = 1500 * 1000 // 25 minutes in ms
-//export const TIMER_LENGTH_MS = 5 * 1000
+const BUFFER_SO_THAT_59_ALWAYS_SHOWN = 100
+export const TIMER_LENGTH_MS = 1500 * 1000 + BUFFER_SO_THAT_59_ALWAYS_SHOWN // 25 minutes in ms
+//export const TIMER_LENGTH_MS = 5 * 1000 + BUFFER_SO_THAT_59_ALWAYS_SHOWN
