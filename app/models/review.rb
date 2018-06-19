4c308d29177610b9fc1ee4c5aab4a3c4df1e8763
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2018-06-19 13:12:19 -0400

    Adds excuses, pomodoros, and tasks to review cal

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,16 +10,36 @@ class Review
 
     daily_goals = DailyGoal.all_for_user(user)
 
+    created_at_date = -> (record) { record.created_at.to_date }
+
     accomplishments_by_day =
       user.accomplishments
           .where(created_at: month_query_range)
-          .group_by { |a| a.created_at.to_date }
+          .group_by(&created_at_date)
+
+    excuses_by_day =
+      user.excuses
+          .where(created_at: month_query_range)
+          .group_by(&created_at_date)
+
+    tasks_completed_by_day =
+      user.tasks
+          .where(completed_at: month_query_range)
+          .group_by { |task| task.completed_at.to_date }
+
+    pomodoros_completed_by_day =
+      user.pomodoros
+          .where(created_at: month_query_range)
+          .group_by(&created_at_date)
 
     month_query_range.map do |day|
       self.new(
         date: day,
         accomplishments: accomplishments_by_day[day] || [],
-        daily_goals: daily_goals.select { |dg| day >= dg.created_at.to_date }
+        daily_goals: daily_goals.select { |dg| day >= dg.created_at.to_date },
+        excuses: excuses_by_day[day] || [],
+        tasks: tasks_completed_by_day[day] || [],
+        pomodoros: pomodoros_completed_by_day[day] || []
       )
     end
   end
